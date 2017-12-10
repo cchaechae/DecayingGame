895879f145f926c0e5d3afa746e94301adda59ed
MomalBaloch <baloc22m@mtholyoke.edu>
2017-12-10 10:36:39 -0500

    .
    ..

--- src/RainDemo.java
+++ src/RainDemo.java
@@ -1,9 +1,18 @@
+import java.util.Random;
+
 import processing.core.PApplet;
 import processing.core.PVector;
 
 public class RainDemo extends PApplet{
 
 	Rain[] rain;
+	Rain[] rainOne;
+	Rain[] rainTwo;
+	Rain[] rainThree;
+	
+	
+	int secWidth = floor(this.width/4);
+	
 	
 	public void settings() {
 		size(600,800);
@@ -11,19 +20,74 @@ public class RainDemo extends PApplet{
 		
 	public void setup(){
 		background(0);
-		rain = new Rain[50];
+		
+		
+		rain = new Rain[25];
+		rainOne = new Rain[100];
+		rainTwo = new Rain[10];
+		rainThree = new Rain[50];
+		
+	
+		int[] arr = {0,150,300,450};
+		
+		
+		//int rand = getRandom(arr);
+		for (int i = 0; i < 25; i++) {
+			
+		
+			rain[i] = new Rain(this, (int)random(0 ,150), (int)random(0, this.height), (int)random(2, 4));
+		}
+		
+		for (int i = 0; i < 100; i++) {
+			
+			rainOne[i] = new Rain(this, (int)random( 150, 300), (int)random(0, this.height), (int)random(2, 4));
+		}
+	
+		for (int i = 0; i < 10; i++) {
+			
+			rainTwo[i] = new Rain(this, (int)random(300, 450), (int)random(0, this.height), (int)random(2, 4));
+		}
 		
 		for (int i = 0; i < 50; i++) {
-			rain[i] = new Rain(this, (int)random(0, this.width), (int)random(0, this.height), (int)random(2, 4));
+			
+			rainThree[i] = new Rain(this, (int)random(450, 600), (int)random(0, this.height), (int)random(2, 4));
 		}
 
 	}
+	public static int getRandom(int[] array) {
+	    int rnd = new Random().nextInt(array.length);
+	    return array[rnd];
+	}
+
 			
 	public void draw(){
+		
+		
+		
 		background(0);
-		for (int i = 0; i < 50; i++) {
-			rain[i].draw();
+		
+		
+		fill(204, 102, 0);
+		rect(300, 400, 100, 60);
+		
+		
+		
+		
+		
+		for (int i = 0; i < 25 ; i++) {
+			rain[i].draw(300,400,400);
 		}
+		for (int i = 0; i < 100 ; i++) {
+			rainOne[i].draw(300,400,400);
+		}
+		for (int i = 0; i < 10 ; i++) {
+			rainTwo[i].draw(300,400,400);
+		}
+		for (int i = 0; i < 50 ; i++) {
+			rainThree[i].draw(300,400,400);
+		}
+		
+		
 	}
 		
 	public static void main(String[] args) {
--- src/UrbanDecay.java
+++ src/UrbanDecay.java
@@ -4,140 +4,151 @@ import processing.core.PApplet;
 //import processing.core.PShape;
 import processing.core.PVector;
 
+public class UrbanDecay extends PApplet {
 
-public class UrbanDecay extends PApplet{
-	
-	float radx;   // Radius
+	float radx; // Radius
 	float rady;
 	float angle1; // angle
-	float x;      // result
+	float x; // result
 	float y;
 	float minus = 0.05f;
 	Umbrella umbrella;
 	final static int NUM_PARTICLE = 1000;
 	final static int NUM_BUILDING = 10;
-	final static float ZOOM = 0.5f;		
-	float bottom = -PROJECTOR_RATIO/ZOOM;
-;
+	final static float ZOOM = 0.5f;
+	float bottom = -PROJECTOR_RATIO / ZOOM;
+
+	Rain[] rain;
+	Rain[] rainOne;
+	Rain[] rainTwo;
+	Rain[] rainThree;
+
 	Building[] building = new Building[NUM_BUILDING];
 	Particle[] field = new Particle[NUM_PARTICLE];
 
 	KinectBodyDataProvider kinectReader;
-	public static float PROJECTOR_RATIO = 1080f/1920.0f;
-	
+	public static float PROJECTOR_RATIO = 1080f / 1920.0f;
+
 	public void createWindow(boolean useP2D, boolean isFullscreen, float windowsScale) {
 		if (useP2D) {
-			if(isFullscreen) {
-				fullScreen(P2D);  			
+			if (isFullscreen) {
+				fullScreen(P2D);
 			} else {
-				size((int)(1920 * windowsScale), (int)(1080 * windowsScale), P2D);
+				size((int) (1920 * windowsScale), (int) (1080 * windowsScale), P2D);
 			}
 		} else {
-			if(isFullscreen) {
-				fullScreen();  			
+			if (isFullscreen) {
+				fullScreen();
 			} else {
-				size((int)(1920 * windowsScale), (int)(1080 * windowsScale));
+				size((int) (1920 * windowsScale), (int) (1080 * windowsScale));
 			}
-		}		
+		}
 	}
-	
+
 	// use lower numbers to zoom out (show more of the world)
 	// zoom of 1 means that the window is 2 meters wide and appox 1 meter tall.
 	public void setScale(float zoom) {
-		scale(zoom* width/2.0f, zoom * -width/2.0f);
-		translate(1f/zoom , -PROJECTOR_RATIO/zoom );		
+		scale(zoom * width / 2.0f, zoom * -width / 2.0f);
+		translate(1f / zoom, -PROJECTOR_RATIO / zoom);
 	}
 
 	public void settings() {
 		createWindow(false, false, .5f);
 	}
 
-	public void setup(){
-		
+	public void setup() {
+
 		/*
-		 * use this code to run your PApplet from data recorded by UPDRecorder 
+		 * use this code to run your PApplet from data recorded by UPDRecorder
 		 */
 		/*
-		try {
-			kinectReader = new KinectBodyDataProvider("test.kinect", 10);
-		} catch (IOException e) {
-			System.out.println("Unable to creat e kinect producer");
-		}
+		 * try { kinectReader = new KinectBodyDataProvider("test.kinect", 10); }
+		 * catch (IOException e) {
+		 * System.out.println("Unable to creat e kinect producer"); }
 		 */
-		
+
 		try {
 			kinectReader = new KinectBodyDataProvider("exitTest.kinect", 10);
 		} catch (IOException e) {
 			System.out.println("Unable to creat e kinect producer");
 		}
-		//kinectReader = new KinectBodyDataProvider(8008);
+		// kinectReader = new KinectBodyDataProvider(8008);
 		kinectReader.start();
-		
+
+		// rain
+
+	
 		float x = 0.2f;
-		for(int i = 0; i < NUM_BUILDING; i++){
-			if(i <= 3)
-				building[i] = new Building(this, -2f + x, bottom, random(0.1f, .4f), random(0.2f, 0.7f)); 
-			else if(i <= 7)
-				building[i] = new Building(this, -2f + x, bottom, random(0.1f, .4f), random(0.5f, 1.5f)); 
-			else if(i <= 9)
+		for (int i = 0; i < NUM_BUILDING; i++) {
+			if (i <= 3)
+				building[i] = new Building(this, -2f + x, bottom, random(0.1f, .4f), random(0.2f, 0.7f));
+			else if (i <= 7)
+				building[i] = new Building(this, -2f + x, bottom, random(0.1f, .4f), random(0.5f, 1.5f));
+			else if (i <= 9)
 				building[i] = new Building(this, -2f + x, bottom, random(0.1f, .4f), random(0.2f, 0.7f));
 			else
 				building[i] = new Building(this, -2f + x, bottom, random(0.1f, .4f), random(0.2f, 0.4f));
 			x += 0.4f;
 		}
-		
-		
-		for (int i=0; i < 100; i++) {
-			field[i] = new Particle(this, x, y, color(random(0,255),random(0,255),random(0,255)));
-		 }
+
+		for (int i = 0; i < 100; i++) {
+			field[i] = new Particle(this, x, y, color(random(0, 255), random(0, 255), random(0, 255)));
+		}
+
 	}
-	
-	public void draw(){
-		setScale(ZOOM);
+
+	public void draw() {
 		
-		noStroke();
+
+	
 		background(0);
 		
-		fill(150, 150, 150);
-//		drawBuilding(-1.8f, 0.6f, 0.2f);
-//		drawBuilding(-1.4f, 0.73f, 0.35f);
-//		drawBuilding(-0.9f, 1.1f, 0.15f);
-//		drawBuilding(-.5f, 1.6f, 0.25f);
-//		drawBuilding(.2f, 0.73f, 0.45f);
-//		drawBuilding(.8f, 0.86f, 0.15f);
-//		drawBuilding(1.4f, 0.73f, 0.2f);
 		
-		fill(255, 255, 255);
-		//ground
-		rect(-2f, bottom, 4f, 0.1f);
-		//white buildings
+		
+		setScale(ZOOM);
+		noStroke();
+		
+		fill(150, 150, 150);
+		
+		// drawBuilding(-1.8f, 0.6f, 0.2f);
+		// drawBuilding(-1.4f, 0.73f, 0.35f);
+		// drawBuilding(-0.9f, 1.1f, 0.15f);
+		// drawBuilding(-.5f, 1.6f, 0.25f);
+		// drawBuilding(.2f, 0.73f, 0.45f);
+		// drawBuilding(.8f, 0.86f, 0.15f);
+		// drawBuilding(1.4f, 0.73f, 0.2f);
 
-		for(int i = 0; i < NUM_BUILDING; i++){
+		fill(255, 255, 255);
+		// ground
+		rect(-2f, bottom, 4f, 0.1f);
+		// white buildings
+
+		for (int i = 0; i < NUM_BUILDING; i++) {
 			building[i].draw();
 		}
-		
-//		drawBuilding(-1.9f, 0.4f, 0.2f);
-//		drawBuilding(-1.6f, 0.5f, 0.18f);
-//		drawBuilding(-1.36f, 0.35f, 0.4f);
-//		drawBuilding(-0.8f, 0.7f, 0.24f);
-//		drawBuilding(-0.76f, 0.83f, 0.16f);
-//		drawBuilding(-0.52f, 0.5f, 0.2f);
-//		drawBuilding(-0.69f, 1f, 0.02f);
-//		drawBuilding(-0.2f, 0.67f, 0.13f);
-//		drawBuilding(0, 1.1f, 0.35f);
-//		drawBuilding(0.5f, 0.3f, 0.8f);
-//		drawBuilding(0.9f, 0.6f, 0.45f);
-//		drawBuilding(1.5f, 0.25f, 0.2f);
-		
-	    //rect(x,y+20,5,50);
-	    
-		//shape(city, -2f, bottom);
+			
 
+		// drawBuilding(-1.9f, 0.4f, 0.2f);
+		// drawBuilding(-1.6f, 0.5f, 0.18f);
+		// drawBuilding(-1.36f, 0.35f, 0.4f);
+		// drawBuilding(-0.8f, 0.7f, 0.24f);
+		// drawBuilding(-0.76f, 0.83f, 0.16f);
+		// drawBuilding(-0.52f, 0.5f, 0.2f);
+		// drawBuilding(-0.69f, 1f, 0.02f);
+		// drawBuilding(-0.2f, 0.67f, 0.13f);
+		// drawBuilding(0, 1.1f, 0.35f);
+		// drawBuilding(0.5f, 0.3f, 0.8f);
+		// drawBuilding(0.9f, 0.6f, 0.45f);
+		// drawBuilding(1.5f, 0.25f, 0.2f);
 
-//		KinectBodyData bodyData = kinectReader.getMostRecentData();
+		// rect(x,y+20,5,50);
+
+		// shape(city, -2f, bottom);
+
+		// KinectBodyData bodyData = kinectReader.getMostRecentData();
 		KinectBodyData bodyData = kinectReader.getData();
 		Body person = bodyData.getPerson(0);
-		if(person != null){
+		if (person != null) {
 			PVector head = person.getJoint(Body.HEAD);
 			PVector spine = person.getJoint(Body.SPINE_SHOULDER);
 			PVector spineBase = person.getJoint(Body.SPINE_BASE);
@@ -148,88 +159,93 @@ public class UrbanDecay extends PApplet{
 			PVector handLeft = person.getJoint(Body.HAND_LEFT);
 			PVector handRight = person.getJoint(Body.HAND_RIGHT);
 
-			fill(255,255,255);
+			fill(255, 255, 255);
 			noStroke();
-			umbrella = new Umbrella (this, 0.5f, head);
-			if (head != null){
+			umbrella = new Umbrella(this, 0.5f, head);
+			if (head != null) {
 				umbrella.drawUmbrella();
 			}
-			
-			drawIfValid(head);
 
-//			drawIfValid(spine);
-//			drawIfValid(spineBase);
-//			drawIfValid(shoulderLeft);
-//			drawIfValid(shoulderRight);
-//			drawIfValid(footLeft);
-//			drawIfValid(footRight);
-//			drawIfValid(handLeft);
-//			drawIfValid(handRight);
+			// drawIfValid(head);
 
-//			if(head != null){
-//				for (int i=0; i < NUM_PARTICLE; i++) {
-//					if(!field[i].clicked){
-//						field[i].update(field[i].location.x+head.x, field[i].location.y+head.y);
-//						field[i].draw();
-//						field[i].update(field[i].location.x-head.x, field[i].location.y-head.y);
-//						System.out.println(field[i].location.x);
-//					}
-//					else{
-//						field[i].falling();
-//					}
-//										
-//					System.out.println(field[i].location.x);
-//				}
-//			}
-//			
-//			if(mousePressed){
-//				for(int i=0; i < field.length; i++){
-//					if(field[i].location.y < head.y+0.05f){
-//						System.out.println(field[i].location.y);
-//						System.out.println(head.y+0.05f);
-//						
-//						field[i].mouseClicked();
-//					}
-//				}
-//				//minus = minus - 0.001f;
-//			}
+			// drawIfValid(spine);
+			// drawIfValid(spineBase);
+			// drawIfValid(shoulderLeft);
+			// drawIfValid(shoulderRight);
+			// drawIfValid(footLeft);
+			// drawIfValid(footRight);
+			// drawIfValid(handLeft);
+			// drawIfValid(handRight);
 
-			
-//			if( 
-//					(footRight != null) &&
-//					(footLeft != null) &&
-//					(handLeft != null) &&
-//					(handRight != null) 
-//					) {
-//				stroke(255,0,0, 100);
-//				noFill();
-//				strokeWeight(.05f); // because of scale weight needs to be much thinner
-//				curve(
-//						footLeft.x, footLeft.y, 
-//						handLeft.x, handLeft.y, 
-//						handRight.x, handRight.y,
-//						footRight.x, footRight.y
-//						);
-//			}
+			// if(head != null){
+			// for (int i=0; i < NUM_PARTICLE; i++) {
+			// if(!field[i].clicked){
+			// field[i].update(field[i].location.x+head.x,
+			// field[i].location.y+head.y);
+			// field[i].draw();
+			// field[i].update(field[i].location.x-head.x,
+			// field[i].location.y-head.y);
+			// System.out.println(field[i].location.x);
+			// }
+			// else{
+			// field[i].falling();
+			// }
+			//
+			// System.out.println(field[i].location.x);
+			// }
+			// }
+			//
+			// if(mousePressed){
+			// for(int i=0; i < field.length; i++){
+			// if(field[i].location.y < head.y+0.05f){
+			// System.out.println(field[i].location.y);
+			// System.out.println(head.y+0.05f);
+			//
+			// field[i].mouseClicked();
+			// }
+			// }
+			// //minus = minus - 0.001f;
+			// }
+
+			// if(
+			// (footRight != null) &&
+			// (footLeft != null) &&
+			// (handLeft != null) &&
+			// (handRight != null)
+			// ) {
+			// stroke(255,0,0, 100);
+			// noFill();
+			// strokeWeight(.05f); // because of scale weight needs to be much
+			// thinner
+			// curve(
+			// footLeft.x, footLeft.y,
+			// handLeft.x, handLeft.y,
+			// handRight.x, handRight.y,
+			// footRight.x, footRight.y
+			// );
+			// }
 		}
-	}	
-	
+
+		noStroke();
+
+	}
+
 	/**
-	 * Draws an ellipse in the x,y position of the vector (it ignores z).
-	 * Will do nothing is vec is null.  This is handy because get joint 
-	 * will return null if the joint isn't tracked. 
+	 * Draws an ellipse in the x,y position of the vector (it ignores z). Will
+	 * do nothing is vec is null. This is handy because get joint will return
+	 * null if the joint isn't tracked.
+	 * 
 	 * @param vec
 	 */
 	public void drawIfValid(PVector vec) {
-		if(vec != null) {
-			ellipse(vec.x, vec.y, .1f,.1f);
+		if (vec != null) {
+			ellipse(vec.x, vec.y, .1f, .1f);
 		}
 
 	}
-	
 
 	public static void main(String[] args) {
 		PApplet.main(UrbanDecay.class.getName());
 	}
-	
+
 }
